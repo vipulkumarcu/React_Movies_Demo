9114f5a1879602cd3bbd8d58735f81a306514413
vipulkumar.gts@gmail.com <vipulkumar.gts@gmail.com>
2024-07-18 17:42:13 +0530

    sh (8) - Making Apps Error Proof

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,30 +7,100 @@ function App ()
 {
   const [ movies, setMovies ] = useState ( [] );
   const [ isLoading, setIsLoading ] = useState ( false );
+  const [ error, setError ] = useState ( null );
+  const [ retrying, setRetrying ] = useState ( false );
+  const [ intervalId, setIntervalId ] = useState ( null );
+
+  useEffect (
+    () => {
+      return () => {
+        if ( intervalId )
+          {
+          clearInterval ( intervalId );
+        }
+      };
+    }, [ intervalId ] 
+  );
 
   async function fetchMoviesHandler ()
   {
     setIsLoading ( true );
+    setError ( null );
 
-    const response = await fetch ( "https://swapi.dev/api/films" );
+    try
+    {
+      const response = await fetch ( "https://swapi.dev/api/film" );
 
-    const data = await response.json ();
-
-    const transformedMovies = data.results.map (
-      ( movie ) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        };
+      if ( !response.ok )
+      {
+        throw new Error ( "Something Went Wrong... Retrying" )
       }
-    );
 
-    setIsLoading ( false );
+      const data = await response.json ();
+
+      const transformedMovies = data.results.map (
+        ( movie ) => {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            openingText: movie.opening_crawl,
+            releaseDate: movie.release_date,
+          };
+        }
+      );
+
+      setMovies ( transformedMovies );
+
+      setRetrying ( false );
+
+      if ( intervalId )
+      {
+        clearInterval ( intervalId );
+        setIntervalId ( null );
+      }
+    }
+
+    catch ( error )
+    {
+      setError ( error.message );
+      setRetrying ( true );
+      if ( !intervalId )
+      {
+        const id = setInterval ( fetchMoviesHandler, 5000 );
+        setIntervalId ( id );
+      }
+    }
     
-    setMovies ( transformedMovies );
-  }  
+    setIsLoading ( false );
+  }
+
+  function cancelRetryHandler ()
+  {
+    if ( intervalId )
+    {
+      clearInterval ( intervalId );
+      setIntervalId ( null );
+    }
+    setRetrying ( false );
+    setError ( "Retrying canceled by user." );
+  }
+
+  let content = <p> No Movies Found. Click Fetch !! </p>;
+
+  if ( movies.length > 0 )
+  {
+    content = <MoviesList movies = { movies } />
+  }
+
+  if ( error )
+  {
+    content = <p> { error } </p>
+  }
+
+  if ( isLoading )
+  {
+    content = <p> Loading... </p>
+  }
 
   return (
     <React.Fragment>
@@ -39,9 +109,8 @@ function App ()
       </section>
 
       <section>
-      { !isLoading && movies.length > 0 && <MoviesList movies = { movies } /> }
-      { !isLoading && movies.length === 0 && <p> No Movies Found. Click Fetch !! </p> }
-      { isLoading && <p> Loading... </p> }
+        { content }
+        { retrying && <button onClick = { cancelRetryHandler } > Cancel Retry </button> }
       </section>
     </React.Fragment>
   );
