cc3b82ff27507173346559059d612920ba4ae9ec
vipulkumar.gts@gmail.com <vipulkumar.gts@gmail.com>
2024-07-18 16:18:41 +0530

    Sh (6) - Making API calls

--- src/App.js
+++ src/App.js
@@ -6,26 +6,47 @@ import './App.css';
 function App ()
 {
   const [ movies, setMovies ] = useState ( [] );
-  function fetchMoviesHandler ()
-  {
-    fetch ( "https://swapi.dev/api/films" )
-      .then ( response => response.json() )
-        .then ( data => {
-            const transformedMovies = data.results.map (
-              ( movie ) => {
-                return {
-                  id: movie.episode_id,
-                  title: movie.title,
-                  openingText: movie.opening_crawl,
-                  releaseDate: movie.release_date
-                }
-              }
-            )
+  
+  // function fetchMoviesHandler ()
+  // {
+  //   fetch ( "https://swapi.dev/api/films" )
+  //     .then ( response => response.json() )
+  //       .then ( data => {
+  //           const transformedMovies = data.results.map (
+  //             ( movie ) => {
+  //               return {
+  //                 id: movie.episode_id,
+  //                 title: movie.title,
+  //                 openingText: movie.opening_crawl,
+  //                 releaseDate: movie.release_date
+  //               }
+  //             }
+  //           )
 
-            setMovies (transformedMovies);
-          } 
-        )
-  }
+  //           setMovies (transformedMovies);
+  //         } 
+  //       )
+  // }
+
+  async function fetchMoviesHandler ()
+  {
+    const response = await fetch ( "https://swapi.dev/api/films" );
+
+    const data = await response.json ();
+
+    const transformedMovies = data.results.map (
+      ( movie ) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      }
+    );
+
+    setMovies ( transformedMovies );
+  }  
 
   return (
     <React.Fragment>
